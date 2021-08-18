3e8b90306f1bb06854bb6d19fe8bcee54051be75
DESKTOP-K5C0H0N\Muhammad <muhammadbinsohail07@gmail.com>
2021-08-18 21:23:29 +0500

    splashscreen and icon added

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,38 @@
 
+
 import 'package:flutter/material.dart';
+import 'package:my_encore/ui_Pages/login_screen.dart';
 
-import 'ui_Pages/login_screen.dart';
 
-void main() {
-  runApp(MyApp());
+
+void main() { runApp(new MaterialApp( home: new MyApp(), )); }
+
+class MyApp extends StatefulWidget {
+
+  @override _MyAppState createState() => new _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State {
+  @override void initState() {
+    super.initState();
+    new Future.delayed( const Duration(seconds: 5), () =>
+        Navigator.pushReplacement( context,
+          MaterialPageRoute(builder: (context) => LoginScreen()),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'ENCORE',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home:LoginScreen()
-    );
+    return new Scaffold(
+      //backgroundColor: Colors.white,
+        body: Center(
+          child: Container(
+              height: MediaQuery.of(context).size.height*1,
+              width: MediaQuery.of(context).size.width*1,
+              child: Image.asset("assets/images/encore.png",
+                  fit: BoxFit.scaleDown
+              )
+          ),
+        ));
   }
 }
-
